f2b95fe7e70958c6d664f0306c9ba195c92efbdd
DeepSK <deepsk3697@gmail.com>
2026-06-09 22:47:26 +0530

    updated axiosinstance

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { BASE_URL } from '../utils/ip';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || BASE_URL,
   timeout: 15000,
   headers: { 'Content-Type': 'application/json' },
 });
