7055a7056b7c10ae99393babbe36bb12b4722d59
DeepSK <deepsk3697@gmail.com>
2026-06-14 12:06:34 +0530

    updated ip.js

--- src/utils/ip.js
+++ src/utils/ip.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = "http://localhost:5000/api";
-export const BASE_URL = "http://91.134.71.79:5000/api";
+// export const BASE_URL = "http://91.134.71.79:5000/api";
+export const BASE_URL = "https://admin.holidaybooking.be/api";
 
 /** Base URL for static uploads (logos, images). Strips /api from BASE_URL. */
-export const UPLOAD_BASE_URL =
-  BASE_URL.replace(/\/api\/?$/, "");
+export const UPLOAD_BASE_URL = BASE_URL.replace(/\/api\/?$/, "");
